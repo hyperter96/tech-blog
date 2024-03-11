f9ae217e7c4d98f62415327771cfdcd8fa53ecad
liaojunqi <hyperter96@gmail.com>
2024-03-11 17:08:05 +0800

    add: announcement

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -86,41 +86,36 @@ const blogTheme = getThemeConfig({
     nextText: '换一组',
     pageSize: 3,
     empty: '暂无精选内容',
-  }
+  },
   // 公告
-  // popover: {
-  //   title: '公告',
-  //   body: [
-  //     { type: 'text', content: '👇公众号👇---👇 微信 👇' },
-  //     {
-  //       type: 'image',
-  //       src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210'
-  //     },
-  //     {
-  //       type: 'text',
-  //       content: '欢迎大家加群&私信交流'
-  //     },
-  //     {
-  //       type: 'text',
-  //       content: '文章首/文尾有群二维码',
-  //       style: 'padding-top:0'
-  //     },
-  //     {
-  //       type: 'button',
-  //       content: '作者博客',
-  //       link: 'https://sugarat.top'
-  //     },
-  //     {
-  //       type: 'button',
-  //       content: '加群交流',
-  //       props: {
-  //         type: 'success'
-  //       },
-  //       link: 'https://theme.sugarat.top/group.html',
-  //     }
-  //   ],
-  //   duration: 0
-  // },
+  popover: {
+    title: '公告',
+    body: [
+      { type: 'text', content: '👇公众号👇---👇 微信 👇' },
+      {
+        type: 'image',
+        src: 'https://cdn.jsdelivr.net/gh/hyperter96/tech-blog/docs/assets/images/announcement.jpg'
+      },
+      {
+        type: 'text',
+        content: '欢迎大家添加公众号&私信交流'
+      },
+      {
+        type: 'button',
+        content: '云原生文档',
+        link: 'https://ikubevirt.cn'
+      },
+      {
+        type: 'button',
+        content: '添加微信',
+        props: {
+          type: 'success'
+        },
+        link: 'https://cdn.jsdelivr.net/gh/hyperter96/tech-blog/docs/assets/images/wechat-code.jpg',
+      }
+    ],
+    duration: 0
+  },
 })
 
 export { blogTheme }
